7e83c00cf0d3ac12f2c3dc51b25794f3ce65f4d3
Andrea Guido <andrea.guido.research@gmail.com>
2021-09-27 09:08:45 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -20,7 +20,8 @@ dict(
         app_sequence=['public_goods_sorting'],
         treatment = "T1",
         doc=""" <h1>Checklist</h1>
-            <ul> Treatment setup:
+            <ul> Number of subjects: Only multiple of 3 are accepted; sessions must be of 15 subjects. If 30, create 2 sessions.</ul>
+            <ul> Treatment codes:
                 <li>T1 = Endogenous;</li>
                 <li>T2 = Exogenous à la Gacther & Thoni; </li>
                 <li>T3 = Exogenous semi-random; </li> 
@@ -45,18 +46,14 @@ ROOMS = [
     #dict(name='econ101', display_name='EconPsyManagement class', participant_label_file="_room/labels.txt",
     #     use_secure_urls = False),
     #dict(name='live_demo', display_name='Room for live demo (no participant labels)'),
-    dict(name='Angela_SUTAN_class1', display_name='Angela SUTAN Class # 1'),
-    dict(name='Angela_SUTAN_class2', display_name='Angela SUTAN Class # 2'),
-    dict(name='Angela_SUTAN_class3', display_name='Angela SUTAN Class # 3 (extreme case)'),
-    dict(name='Andrea_MART_class1', display_name='Andrea MARTINANGELI Class # 1'),
-    dict(name='Andrea_MART_class2', display_name='Andrea MARTINANGELI Class # 2'),
-    dict(name='Andrea_MART_class3', display_name='Andrea MARTINANGELI Class # 3 (extreme case)'),
-    dict(name='Eli_Sp_class1', display_name='Eli SPIEGELMAN Class # 1'),
-    dict(name='Eli_Sp_class2', display_name='Eli SPIEGELMAN Class # 2'),
-    dict(name='Eli_Sp_class3', display_name='Eli SPIEGELMAN Class # 3 (extreme case)'),
-    dict(name='Andrea_GUIDO_class1', display_name='Andrea GUIDO Class # 1'),
-    dict(name='Andrea_GUIDO_class2', display_name='Andrea GUIDO Class # 2'),
-    dict(name='Andrea_GUIDO_class3', display_name='Andrea GUIDO Class # 3 (extreme case)')
+    dict(name='Angela_SUTAN_class1', display_name='Angela SUTAN #1-15'),
+    dict(name='Angela_SUTAN_class2', display_name='Angela SUTAN Class #16-30'),
+    dict(name='Andrea_MART_class1', display_name='Andrea MARTINANGELI #1-15'),
+    dict(name='Andrea_MART_class2', display_name='Andrea MARTINANGELI #16-30'),
+    dict(name='Eli_Sp_class1', display_name='Eli SPIEGELMAN #1-15'),
+    dict(name='Eli_Sp_class2', display_name='Eli SPIEGELMAN #16-30'),
+    dict(name='Andrea_GUIDO_class1', display_name='Andrea GUIDO #1-15'),
+    dict(name='Andrea_GUIDO_class2', display_name='Andrea GUIDO  #16-30'),
 
 ]
 
